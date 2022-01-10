c5f2d1dd7d2af30e4c55609bb3ca1b4ca63493d2
Licht2050 <mustafa.balkhi.mb@gmail.com>
2022-01-10 23:42:12 +0100

    Election package

--- pkg/Election/Election.go
+++ pkg/Election/Election.go
@@ -1,5 +1,23 @@
 package election
 
 type Election struct {
-	Id int
+	M int
+}
+
+func (e *Election) Add_ID(id int) {
+	e.M = id
+}
+
+func (e *Election) CompaireElection(eN Election) string {
+	if e.M > eN.M {
+		return "gt"
+	} else if e.M == eN.M {
+		return "eq"
+	}
+	return "lt"
+}
+
+func NewElection(id int) *Election {
+
+	return &Election{M: id}
 }
